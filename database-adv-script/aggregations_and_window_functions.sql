6796a733a84a7ff56411c065a946963cca74711f
Simon Chanzu <chanzusimon6@gmail.com>
2025-05-25 16:08:20 +0300

    commits

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,29 +1,32 @@
--- Query 1: Total number of bookings made by each user
-SELECT 
+-- Query to find the total number of bookings made by each user
+SELECT
     u.user_id,
     u.first_name,
     u.last_name,
     COUNT(b.booking_id) AS total_bookings
-FROM 
-    User u
-LEFT JOIN 
-    Booking b ON u.user_id = b.user_id
-GROUP BY 
+FROM
+    User AS u
+LEFT JOIN
+    Booking AS b ON u.user_id = b.user_id
+GROUP BY
     u.user_id, u.first_name, u.last_name
-ORDER BY 
+ORDER BY
     total_bookings DESC;
 
--- Query 2: Rank properties based on total number of bookings using RANK
-SELECT 
+---
+
+-- Query to rank properties based on the total number of bookings they have received
+SELECT
     p.property_id,
     p.name,
-    COUNT(b.booking_id) AS booking_count,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank_row_number,
     RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
-FROM 
-    Property p
-LEFT JOIN 
-    Booking b ON p.property_id = b.property_id
-GROUP BY 
+FROM
+    Property AS p
+LEFT JOIN
+    Booking AS b ON p.property_id = b.property_id
+GROUP BY
     p.property_id, p.name
-ORDER BY 
-    booking_rank;
+ORDER BY
+    total_bookings DESC;
